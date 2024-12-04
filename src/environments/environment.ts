499db73861edf5589db01a046ecda33fb7bd9c35
Amaury Belin <amaury.belin@gmail.com>
2024-12-04 21:52:23 +0100

    chore : preparing app for http connection to test server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  apiUrl: '/api/v1',
+  apiKey: 'MySecret2024!'
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
