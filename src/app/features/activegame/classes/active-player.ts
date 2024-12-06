ec94dbd8ae68ac81bea4ddf69e52f4681cd1ecce
Amaury Belin <amaury.belin@gmail.com>
2024-12-06 11:01:47 +0100

    docs : activePlayer class - reorganizing the class for better readibility and set up basic comments

--- src/app/features/activegame/classes/active-player.ts
+++ src/app/features/activegame/classes/active-player.ts
@@ -3,44 +3,56 @@ import { PlayerService } from "../../players/services/player.service";
 import { PlayedCard } from "../models/activegame.model";
 import { Player } from "../../players/models/player.model";
 
+// This class represents the current active player, and it is used
+// - to handle the data and logic of the player during a gmes (deck, card, can play status, score etc.)
+// - to hold and calculate some handy data like player name, score, etc.
 export class ActivePlayer {
     private playerService = inject(PlayerService);
+    // the other active player, that is used to calculate the score amngst other things
     private otherActivePlayer:ActivePlayer | null = null;
 
-    public setOtherActivePlayer(otherActivePlayer:ActivePlayer) {
-      this.otherActivePlayer = otherActivePlayer;
+    constructor(public gamesRounds:Signal<Array<[PlayedCard, PlayedCard]>>) 
+    {
     }
-
-    public setCardDeck(cardDeck:number[]) {
-      this._cardDeck.set(cardDeck);
-    }
-
+    
+    // the player id, that does not have to be set from start
+    private _id =  signal<number | null>(null);
+    public readonly id = this._id.asReadonly();
+    // we set the player id
     public setPlayerId(id:any) {
       if(typeof id === 'number' || id === null)
         this._id.set(id);
     }
 
-    public updateCanPlayStatus(canPlay:boolean) {
-      this._canPlay.set(canPlay);
+    // CARD DECK
+    // represents the deck of cards that the player has
+    private _cardDeck = signal<number[]>([]); // the value is set in the setCardDeck method, called by the game board
+    public readonly cardDeck = this._cardDeck.asReadonly();
+    // we distribute the cards to the player
+    public setCardDeck(cardDeck:number[]) {
+        this._cardDeck.set(cardDeck);
     }
-
+  
+    // CURRENT CARD
+    // represents the card that the player has picked for the current round
+    private _currentCard = signal<number | null>(null); // the value is set in the pickCard method or via the removeCurrentCardFromHand method
+    public readonly currentCard = this._currentCard.asReadonly();
+    // we remove the current card from the player hand, as a round is completed
     public removeCurrentCardFromHand() {
       this._currentCard.set(null);
     }
 
-    constructor(public gamesRounds:Signal<Array<[PlayedCard, PlayedCard]>>) 
-    {
+    // CAN PLAY
+    // define if the player can play a card in the current round
+    private _canPlay = signal<boolean>(true); // the value is set in the updateCanPlayStatus method, called by the game board
+    public readonly canPlay = this._canPlay.asReadonly();
+    // we update the player can play status
+    public updateCanPlayStatus(canPlay:boolean) {
+      this._canPlay.set(canPlay);
     }
 
-    public pickCard = () => { // will be called by the playerDeck component
-        const cardDeck = this.cardDeck();
-        this._currentCard.update((card:number | null) => cardDeck?.pop() || null);
-        this._cardDeck.update((deck:number[]) => [...cardDeck]);
-      };
-
-    private _id =  signal<number | null>(null); // will be populated by the player selector component
-    public readonly id = this._id.asReadonly();
-
+    // PLAYER NAME
+    // we get the name of the current player, by looking it up in the player service
     public readonly name = computed<string | null>(() => {
       const playerId = this.id();
       if(playerId) {
@@ -49,10 +61,7 @@ export class ActivePlayer {
       return null;
     });
 
-    private _cardDeck = signal<number[]>([]);
-    public readonly cardDeck = this._cardDeck.asReadonly();
-
-     // will be populated by the dealCard method
+    // we calculate the score of the current player, by summing up the number of rounds won
     public readonly score = computed<number>(() => this.gamesRounds().reduce((acc, fight) => 
       {
         const activePlayerCard = fight.find(card => card.playerId === this.id());
@@ -64,6 +73,7 @@ export class ActivePlayer {
       }, 0)
     );
 
+    // we get the potential players that the user can choose from, if the player is not already set up
     public readonly potentialPlayers = computed<Player[]>(() => {
       if(this.otherActivePlayer) 
       {
@@ -72,14 +82,9 @@ export class ActivePlayer {
       return [];
     });
 
-    private _currentCard = signal<number | null>(null);
-    public readonly currentCard = this._currentCard.asReadonly();
-
-     // will be updated by the playerDeck component and the pickCard method
-    private _canPlay = signal<boolean>(true);
-    public readonly canPlay = this._canPlay.asReadonly();
-     // will be updated by the pickCardEffect
+    // we determine if the current player is winning the current  round, and expose it as a signal
     public readonly isWinningRound = computed<boolean>(() => {
+      // it is important to check if both players have a card on the table (otherwise no winner)
       if(this.currentCard() && this.otherActivePlayer?.currentCard())
       {
         return (this.currentCard() || 0) > (this.otherActivePlayer?.currentCard() || 0);
@@ -87,10 +92,23 @@ export class ActivePlayer {
       return false;
     });
 
+    // we determine if the current player is winning the game, and expose it as a signal
     public readonly isWinning = computed<boolean>(() => {
       if((this.score() ?? 0) > (this.otherActivePlayer?.score() || 0))
         return true;
       else 
         return false;
     })
+
+    // we set the other active player, so current player can calculate its score
+    public setOtherActivePlayer(otherActivePlayer:ActivePlayer) {
+      this.otherActivePlayer = otherActivePlayer;
+    }
+
+    // we ask the player to pick a card and update its current card
+    public pickCard = () => {
+      const cardDeck = this.cardDeck();
+      this._currentCard.update((card:number | null) => cardDeck?.pop() || null);
+      this._cardDeck.update((deck:number[]) => [...cardDeck]);
+    };
 }
