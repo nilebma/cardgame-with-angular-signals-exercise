541b0618059d03e2cea97adb6f1694b4082234ef
Amaury Belin <amaury.belin@gmail.com>
2024-12-06 11:34:06 +0100

    docs : activeGame page - basic comments (to be completed with AI)

--- src/app/pages/activegame/activegame.page.ts
+++ src/app/pages/activegame/activegame.page.ts
@@ -6,6 +6,9 @@ import { AlertController } from '@ionic/angular';
 import { GameBoardComponent } from 'src/app/features/activegame/ui/game-board/game-board.component';
 import { GameState } from 'src/app/features/activegame/models/activegame.model';
 
+// this page present an active game and let two player play the game.
+// The game is presented via the game-board component. 
+// Here we handle the toolbar and the exit dialog if user wants to quit the game while it is in progress
 @Component({
   selector: 'app-activegame',
   templateUrl: './activegame.page.html',
@@ -23,22 +26,30 @@ import { GameState } from 'src/app/features/activegame/models/activegame.model';
 })
 export class ActivegamePage {
 
+  // DEPENDENCIES
   private alertController = inject(AlertController);
   private navCtrl = inject(NavController);
 
+  // GAME STATE
+  // represents the current state of the game, it is re
   gameState = signal<GameState | null>(null);
-
   setGameState(state:any) {
     console.log("setGameState", state);
     if(state && typeof state === 'string')
       this.gameState.set(state as GameState);
   }
 
+  // EXIT PAGE
+  // when the game is saved, we go back to the home page
   gobackWhenGameIsSaved = effect(() => {
     if(this.gameState() === 'saved')
       this.goBackToHomePage();
   });
+  private goBackToHomePage() {
+    this.navCtrl.navigateBack('/home');
+  }
 
+  // method used by guard to check if the page can be deactivated
   async canDeactivate() {
     console.log("canDeactivate", this.gameState());
     if(this.gameState() === 'playerSelection' || this.gameState() === 'saved')
@@ -50,6 +61,7 @@ export class ActivegamePage {
     }
   }
 
+  // method used by guard to show the exit dialog, when the page is about to be deactivated
   async showExitDialog() {
     const alert = await this.alertController.create({
       header: 'Quitter la partie',
@@ -68,9 +80,4 @@ export class ActivegamePage {
     await alert.present();
     return await alert.onDidDismiss();
   }
-
-  private goBackToHomePage() {
-    this.navCtrl.navigateBack('/home');
-  }
-
 }
